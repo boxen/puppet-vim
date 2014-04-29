a1ecbaee217e1b4851a0ae8c916cf335f1cb0e82
Pan Luo <xcompass@gmail.com>
2014-04-29 11:43:36 -0700

    Compatible with Spf13Vim3 module
    Without the change, this module will delete the symbol link ~/.vim
    created by spf13vim and replace it with a directory. All the bundles
    installed will be gone.

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,24 +27,27 @@ class vim {
     provider => pip,
   }
 
-  file { [$vimdir,
-    "${vimdir}/autoload",
-    "${vimdir}/bundle"]:
-    ensure  => directory,
-    recurse => true,
+  if ! defined(Class['spf13vim3']) {
+    file { [$vimdir,
+      "${vimdir}/autoload",
+      "${vimdir}/bundle"]:
+      ensure  => directory,
+      recurse => true,
+    }
+
+    repository { "${vimdir}/vim-pathogen":
+      source => 'tpope/vim-pathogen'
+    }
+
+    file { "${vimdir}/autoload/pathogen.vim":
+      target  => "${vimdir}/vim-pathogen/autoload/pathogen.vim",
+      require => [
+        File[$vimdir],
+        File["${vimdir}/autoload"],
+        File["${vimdir}/bundle"],
+        Repository["${vimdir}/vim-pathogen"]
+      ]
+    }
   }
 
-  repository { "${vimdir}/vim-pathogen":
-    source => 'tpope/vim-pathogen'
-  }
-
-  file { "${vimdir}/autoload/pathogen.vim":
-    target  => "${vimdir}/vim-pathogen/autoload/pathogen.vim",
-    require => [
-      File[$vimdir],
-      File["${vimdir}/autoload"],
-      File["${vimdir}/bundle"],
-      Repository["${vimdir}/vim-pathogen"]
-    ]
-  }
 }
