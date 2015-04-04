3fffd1440f257035210140426b6b165b0ddc2a84
Pan Luo <xcompass@gmail.com>
2015-04-03 22:58:58 -0700

    Remove docutils and mercurial
    They are not needed to install brew vim anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,19 +13,7 @@ class vim {
   $vimrc = "${home}/.vimrc"
   $vimdir = "${home}/.vim"
 
-  package { 'vim':
-    require => Package['mercurial']
-  }
-  # Install mercurial since the vim brew package don't satisfy the requirement (vim is fetched using $ hg)
-  package { 'mercurial':
-    require => Package['docutils']
-  }
-  # docutils is required by mercurial.
-  # https://github.com/boxen/puppet-vim/issues/12
-  package { 'docutils':
-    ensure   => installed,
-    provider => pip,
-  }
+  package { 'vim': }
 
   if ! defined(Class['spf13vim3']) {
     file { [$vimdir,
